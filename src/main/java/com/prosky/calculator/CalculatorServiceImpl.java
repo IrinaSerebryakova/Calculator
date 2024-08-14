a4b68a026a0de8b9a4ce91d9d6144719590d1e1c
Iserebryakova <ia.serebr@gmail.com>
2024-08-14 22:34:53 +0300

    Исправленная версия

--- src/main/java/com/prosky/calculator/CalculatorServiceImpl.java
+++ src/main/java/com/prosky/calculator/CalculatorServiceImpl.java
@@ -4,21 +4,25 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Service
-public class CalculatorServiceImpl implements CalculatorService{
+public class CalculatorServiceImpl implements CalculatorService {
     private CalculatorServiceImpl calculatorService;
+
     public void Controller(CalculatorServiceImpl calculatorService) {
         this.calculatorService = calculatorService;
     }
+
     @Override
     public String welcome() {
-            return "Добро пожаловать в калькулятор";
-        }
+        return "Добро пожаловать в калькулятор";
+    }
+
     @Override
-    public String plus(@RequestParam("num1&num2") Integer num1, Integer num2){
+    public String plus(@RequestParam Integer num1, @RequestParam Integer num2) {
         return String.format("%d + %d = %d", num1, num2, num1 + num2);
-        }
+    }
+
     @Override
-    public String minus(@RequestParam("num1&num2") Integer num1, Integer num2){
+    public String minus(@RequestParam Integer num1, @RequestParam Integer num2) {
         if (Integer.toString(num1).equals("") || Integer.toString(num2).equals("")) {
             return "Error";
         }
@@ -26,18 +30,20 @@ public class CalculatorServiceImpl implements CalculatorService{
     }
 
     @Override
-    public String multiply(@RequestParam("num1&num2") Integer num1, Integer num2){
+    public String multiply(@RequestParam Integer num1, @RequestParam Integer num2) {
         if (Integer.toString(num1).equals("") || Integer.toString(num2).equals("")) {
             return "Error";
         }
         return String.format("%d * %d = %d", num1, num2, num1 * num2);
     }
-    @Override
-    public String divide(@RequestParam("num1&num2") Integer num1, Integer num2) {
-        if ((Integer.toString(num1).equals("") || Integer.toString(num2).equals("")) || num2 == 0){
-           return "Error";
-        }else if (num1 != 0 && num2 != 0) {
 
-        } return String.format("%d / %d = %d", num1, num2, num1 / num2);
+    @Override
+    public String divide(@RequestParam Integer num1, @RequestParam Integer num2) {
+        if (num2 == 0) {
+            return "Error";
+        } else if (num1 != 0 && num2 != 0) {
+
+        }
+        return String.format("%d / %d = %d", num1, num2, num1 / num2);
     }
 }
--- src/main/java/com/prosky/calculator/Controller.java
+++ src/main/java/com/prosky/calculator/Controller.java
@@ -1,4 +1,5 @@
 package com.prosky.calculator;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,27 +11,29 @@ public class Controller {
     public Controller(CalculatorServiceImpl calculatorService) {
         this.calculatorService = calculatorService;
     }
-    @RequestMapping (path="/calculator")
+
+    @RequestMapping(path = "/calculator")
     public String welcome() {
         return calculatorService.welcome();
     }
-    @RequestMapping (path="/calculator/plus")
-    public String plus(Integer num1, Integer num2){
+
+    @RequestMapping(path = "/calculator/plus")
+    public String plus(Integer num1, Integer num2) {
         return calculatorService.plus(num1, num2);
     }
 
-    @RequestMapping (path="/calculator/minus")
-    public String minus(Integer num1, Integer num2){
+    @RequestMapping(path = "/calculator/minus")
+    public String minus(Integer num1, Integer num2) {
         return calculatorService.minus(num1, num2);
     }
 
-    @RequestMapping (path="/calculator/multiply")
-    public String multiply(Integer num1, Integer num2){
+    @RequestMapping(path = "/calculator/multiply")
+    public String multiply(Integer num1, Integer num2) {
         return calculatorService.multiply(num1, num2);
     }
 
-    @RequestMapping (path="/calculator/divide")
-    public String divide(Integer num1, Integer num2){
+    @RequestMapping(path = "/calculator/divide")
+    public String divide(Integer num1, Integer num2) {
         return calculatorService.divide(num1, num2);
     }
 
