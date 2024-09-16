4da0ff3733918099518f0019f00dca64ea563f5d
SerebryakovaIA <ia.serebr@gmail.com>
2024-09-16 17:11:44 +0300

    Покрытие калькулятора тестами

---
+++ src/test/java/com/prosky/calculator/CalculatorServiceImplTestConstants.java
@@ -0,0 +1,8 @@
+package com.prosky.calculator;
+
+public class CalculatorServiceImplTestConstants {
+    public static final String NUMBER_1 = "15";
+    public static final String NUMBER_2 = "5";
+    public static final String CORRECT_MESSAGE = "The data for the work is entered correctly! :-)";
+    public static final String DEFAULT_MESSAGE = "Cannot parse null string";
+}
